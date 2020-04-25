9a6c7d6f35c6a55cfc3719ac60cb3540174ad27f
Patrick O'Leary <patrick.oleary@gmail.com>
2020-04-25 14:44:28 -0500

    update Twisted so we can use "-slim" base
    The very old version of Twisted originally specified (naturally) did
    not have a wheel on PyPI, so needed to be built from source, in turn
    requiring the normal Python image rather than permitting use of the
    much smaller "-slim" image. Twisted has been surprisingly API stable
    for the past seven years and we can just use the latest version, which
    does have a wheel.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:2
+FROM python:2-slim
 
 COPY requirements.txt requirements.txt
 
--- src/setup.py
+++ src/setup.py
@@ -7,10 +7,10 @@ setup(
     
     install_requires = """
         PyYAML==3.10
-        Twisted==12.3.0
+        Twisted==20.3.0
         autobahn==0.5.9
         pystache==0.5.3
-        zope.interface==4.0.3
+        zope.interface==4.4.2
     """,
     
     include_package_data = True,
