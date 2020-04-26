23d2e0140bb03b8c2f4ea836e235eb72009312c6
Patrick O'Leary <patrick.oleary@gmail.com>
2020-04-25 22:01:51 -0500

    make card data path configurable
    Makes it easy to keep decks in a persistent Docker volume.

--- src/configuration.py
+++ src/configuration.py
@@ -1,5 +1,7 @@
 import os
 
+ABS_PATH = os.path.dirname(os.path.realpath(__file__))
+
 
 class Configuration(object):
     def __init__(self, **kwargs):
@@ -18,6 +20,9 @@ class Configuration(object):
             "CAH_SERVER_PROXY_PORT", os.getenv("CAH_PORT", 8765)
         )
         self.server_interface = os.getenv("CAH_SERVER_INTERFACE", "")
+        self.card_data_path = os.getenv(
+            "CAH_CARD_DATA_PATH", os.path.join(ABS_PATH, "data")
+        )
 
     def __getitem__(self, *args, **kwargs):
         return self.__dict__.__getitem__(*args, **kwargs)
