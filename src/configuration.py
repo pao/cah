84d14faa928fd87da5e00a2544fdf68d37c3845c
Patrick O'Leary <patrick.oleary@gmail.com>
2020-04-25 22:31:12 -0500

    add docker-compose file
    Adds a configuration for Docker Compose which creates a persistent
    volume for storing deck files.

--- src/configuration.py
+++ src/configuration.py
@@ -12,12 +12,14 @@ class Configuration(object):
         self.server_domain = os.getenv(
             "CAH_SERVER_DOMAIN", os.getenv("CAH_DOMAIN", "localhost")
         )
-        self.websocket_port = os.getenv(
-            "CAH_WEBSOCKET_PORT", os.getenv("CAH_PORT", 8765)
+        self.websocket_port = int(
+            os.getenv("CAH_WEBSOCKET_PORT", os.getenv("CAH_PORT", 8765))
         )
-        self.server_port = os.getenv("CAH_SERVER_PORT", os.getenv("CAH_PORT", 8765))
-        self.server_proxy_port = os.getenv(
-            "CAH_SERVER_PROXY_PORT", os.getenv("CAH_PORT", 8765)
+        self.server_port = int(
+            os.getenv("CAH_SERVER_PORT", os.getenv("CAH_PORT", 8765))
+        )
+        self.server_proxy_port = int(
+            os.getenv("CAH_SERVER_PROXY_PORT", os.getenv("CAH_PORT", 8765))
         )
         self.server_interface = os.getenv("CAH_SERVER_INTERFACE", "")
         self.card_data_path = os.getenv(
